b09d3fa7b379621b84d6ba73cb73fc9b05b06a31
Alexey Zankevich <zancudero@gmail.com>
2016-10-18 16:11:14 +0300

    Added stub test

--- tests/django_sample/sampleapp/tests.py
+++ tests/django_sample/sampleapp/tests.py
@@ -4,20 +4,35 @@ from pprint import pprint
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from tests.django_sample.sampleapp.models import Author, Article
+
 
 class MainTest(TestCase):
-    def test_list_articles(self):
-        url = '/article/{}/'.format(self.articles[0].id)
-        query = 'article_fields=id,author&author_fields=id,name,birth_date'
+    def setUp(self):
+        self.client = APIClient()
+
+        self.author1 = Author.objects.create(name='author 1',
+                                             birth_date=date(1985, 1, 31))
+
+        self.author2 = Author.objects.create(name='author 2',
+                                             birth_date=date(1986, 2, 20))
+
+        self.articles = []
+
+        for i in range(5):
+            for author in [self.author1, self.author2]:
+                article = Article.objects.create(title='article {} by {}'.format(i, author.name),
+                                                 content='article content {}'.format(i),
+                                                 author=author)
+                self.articles.append(article)
+
+    def test_get_article_with_extended_fields(self):
+        url = '/article/'.format(self.articles[0].id)
+        query = 'type=review'
 
         response = self.client.get('{}?{}'.format(url, query))
 
         assert response.status_code == 200, response.status_code
 
         response_json = response.json()
-        assert 'author' in response_json, response_json
-
-        author = response_json['author']
-        assert author == {'id': self.author1.id,
-                          'name': 'author 1',
-                          'birth_date': '1985-01-31'}, author
+        assert len(response_json) == 10, response_json
--- tests/django_sample/sampleapp/views.py
+++ tests/django_sample/sampleapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from rest_framework import views, generics, status, serializers, viewsets
 
+from tests.django_sample.sampleapp.models import Article
 
-class FilterView(views):
-    pass
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title']
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
