870da6185f47bbef992af3184e9de483c37c80e6
Kirill Pinchuk <cybergrind@gmail.com>
2016-10-28 23:02:21 +0300

    setup.py: add classifiers and url

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='Various python stuff: testing, aio helpers, etc',
     author='cybergrind',
     author_email='cybergrind@gmail.com',
+    url='http://github.com/tipsi/tipsi_tools',
     keywords=['testing', 'asyncio'],
-    classifiers=[],
     install_requires=[
         'pyyaml>=3.12',
         'python-json-logger==0.1.5',
@@ -18,4 +18,21 @@ setup(
             'tipsi_env_yaml=tipsi_tools.scripts.tipsi_env_yaml:main',
         ]
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
 )
