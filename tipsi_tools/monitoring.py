a1e370ceb1d4c48ab08cf1b5804d4f287a535119
Kirill Pinchuk <cybergrind@gmail.com>
2018-01-26 11:08:58 +0300

    Add monitoring module

---
+++ tipsi_tools/monitoring.py
@@ -0,0 +1,21 @@
+"""
+Monitoring tools are here
+Main goal is to provide easy to use monitoring solution.
+Write monitoring strings to logs, ship them to kibana, extract/aggreate to monitoring system
+"""
+import logging
+
+
+log_mon = logging.getLogger('monitoring')
+
+
+def log_mon_value(name, value=0, **kwargs):
+    """
+    simplest monitoring function to be aggregated with sum
+    """
+    message = '{} => {}'.format(name, value)
+    log_mon.info({'metric_name': name,
+                  'value': value,
+                  'message': message,
+                  **kwargs})
+
