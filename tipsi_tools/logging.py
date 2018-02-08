58747ef8c035806fe19ff12ceeba3b45fb12bdd3
Kirill Pinchuk <cybergrind@gmail.com>
2018-02-09 00:47:47 +0300

    Easy log setup with: logging.setup_logger

--- tipsi_tools/logging.py
+++ tipsi_tools/logging.py
@@ -1,4 +1,5 @@
 import os
+import logging.config
 
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
@@ -33,4 +34,47 @@ class JSFormatter(JsonFormatter):
     def process_log_record(self, rec):
         rec.update(self.default_keys)
         return rec
+
+
+def setup_logger(root_dir, base_name, enable_json=True):
+    JSON_FORMATTER = {
+        '()': 'tipsi_tools.logging.JSFormatter',
+        'env_vars': ['HOST_TYPE', 'TIPSI_CONFIG', 'TIPSI_BRANCH', 'CONTAINER_TYPE'],
+    }
+
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'json': JSON_FORMATTER,
+            'standard': {'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'},
+        },
+        'handlers': {
+            'default': {
+                'level': 'INFO',
+                'class': 'safe_logger.TimedRotatingFileHandlerSafe',
+                'filename': os.path.join(root_dir, '{}.log'.format(base_name)),
+                'when': 'midnight',
+                'interval': 1,
+                'backupCount': 30,
+                'formatter': 'standard',
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        }
+    }
+    if enable_json:
+        LOGGING['handlers']['json'] = {
+            'formatter': 'json',
+            'level': 'DEBUG',
+            'class': 'safe_logger.TimedRotatingFileHandlerSafe',
+            'filename': os.path.join(root_dir, '{}_json.log'.format(base_name)),
+        }
+        LOGGING['loggers']['']['handlers'].append('json')
+    logging.config.dictConfig(LOGGING)
 
