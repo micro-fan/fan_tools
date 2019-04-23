eabfcfc6c2c81f814647d56f0f34606ee447b954
Kirill Pinchuk <cybergrind@gmail.com>
2019-04-23 15:40:57 +0300

    Add TrigramFilterField

--- tipsi_tools/__init__.py
+++ tipsi_tools/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '1.41.0'
+__version__ = '1.42.0'
 
 import tipsi_tools.tipsi_logging as logging  # noqa
--- tipsi_tools/drf/filters.py
+++ tipsi_tools/drf/filters.py
@@ -2,7 +2,10 @@ import logging
 from functools import reduce
 
 import django_filters
-from django.db.models import Q
+from django.db.models import Q, F, Value
+
+from tipsi_tools.django.db.pgfields import WordSimilarity
+from tipsi_tools.django.db.utils import set_word_similarity_threshold
 
 
 class EnumFilter(django_filters.CharFilter):
@@ -36,3 +39,21 @@ class EnumFilter(django_filters.CharFilter):
         except Exception:
             logging.exception('Failed to convert value: {} {}'.format(self_name, name))
             return super(EnumFilter, self).filter(qs, None)
+
+
+class TrigramFilterField(django_filters.CharFilter):
+    def __init__(self, *args, **kwargs):
+        self.similarity_threshold = kwargs.pop('similarity_threshold', None)
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        if value:
+            if self.similarity_threshold:
+                set_word_similarity_threshold(1 - self.similarity_threshold)
+            similarity = WordSimilarity(Value(value), F(self.field_name))
+            qs = (
+                qs.annotate(similarity=similarity)
+                .order_by('similarity', 'pk')
+                .filter(**{f'{self.field_name}__similar': value})
+            )
+        return qs
