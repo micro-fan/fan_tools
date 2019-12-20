32ab8a5a017426ade534571e66ccd9639a8add89
Kirill Pinchuk <cybergrind@gmail.com>
2019-12-20 15:56:04 +0300

    image_utils: fix typo

---
+++ tests/test_image_utils.py
@@ -0,0 +1,15 @@
+import pytest
+from fan_tools.python import py_rel_path
+from fan_tools.image_utils import Transpose
+
+
+@pytest.fixture
+def png_content():
+    with py_rel_path('./image.png').open('rb') as f:
+        yield f.read()
+
+
+def test_png_to_jpg(png_content):
+    out = Transpose().process_binary(png_content, raise_on_open=True)
+    assert len(out) > 0
+    assert out != png_content
