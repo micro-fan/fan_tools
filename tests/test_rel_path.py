7ea5f068adcea16571491f988d55cd392e9b5276
Kirill Pinchuk <cybergrind@gmail.com>
2022-11-12 22:53:36 +0300

    package: migrate to new hatch release. fix tests

--- tests/test_rel_path.py
+++ tests/test_rel_path.py
@@ -8,13 +8,13 @@ from fan_tools.unix import cd
 
 def test_01_rel_path():
     rel_path('./test_rel_path.py', check=True)
-    rel_path('../setup.py', check=True)
+    rel_path('../pyproject.toml', check=True)
 
 
 def test_02_py_rel_path_deprecated():
     with pytest.deprecated_call():
         py_rel_path('./test_rel_path.py', check=True)
-        py_rel_path('../setup.py', check=True)
+        py_rel_path('../pyproject.toml', check=True)
 
 
 def test_03_auto_directory(tmpdir):
